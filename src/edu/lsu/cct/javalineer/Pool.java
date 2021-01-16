9324a8d9a19e267915ce409fb9138c82f06fd50a
Max Morris <maxmorris2001@gmail.com>
2021-01-15 22:50:25 -0600

    Replace custom pool with ForkJoinPool

--- src/edu/lsu/cct/javalineer/Pool.java
+++ src/edu/lsu/cct/javalineer/Pool.java
@@ -7,6 +7,8 @@ package edu.lsu.cct.javalineer;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 /**
@@ -14,21 +16,25 @@ import java.util.function.Supplier;
  * @author sbrandt
  */
 public class Pool {
-    private static final MyPool POOL = new MyPool(Integer.parseInt(System.getProperty("POOL_SIZE","4")));
+    private static final ForkJoinPool POOL = new ForkJoinPool(
+            Integer.parseInt(System.getProperty("POOL_SIZE", "4"))
+    );
+
     static {
-        System.out.println("Pool size: "+POOL);
+        System.out.println("Pool size: " + POOL.getParallelism());
     }
+
     public static void await() {
-        POOL.awaitQuiet();
+        //noinspection StatementWithEmptyBody
+        while (!await(5, TimeUnit.MINUTES)); // todo Probably should do something different
     }
+
+    public static boolean await(long timeout, TimeUnit unit) {
+        return POOL.awaitQuiescence(timeout, unit);
+    }
+
     public static void run(Runnable r) {
-        POOL.add(r);
-    }
-    public static boolean runOne() {
-        return POOL.runOne();
-    }
-    public static int busy() {
-        return POOL.busy;
+        POOL.execute(r);
     }
 
     public static Executor getExecutor() {
@@ -36,6 +42,7 @@ public class Pool {
     }
 
     public static <T> CompletableFuture<T> supply(Supplier<CompletableFuture<T>> supplier) {
-        return POOL.supply(supplier);
+        return CompletableFuture.completedFuture(null)
+                                .thenComposeAsync(x -> supplier.get(), POOL);
     }
 }
